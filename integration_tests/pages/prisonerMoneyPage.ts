c26d6c18ba8aa2ebe22e51220e9190462ce0e4dd
Pietro Convalle <pietro.convalle@madetech.com>
2026-03-09 16:22:23 +0000

    Added integration tests for transactions

--- integration_tests/pages/prisonerMoneyPage.ts
+++ integration_tests/pages/prisonerMoneyPage.ts
@@ -1,8 +1,10 @@
 import { expect, type Locator, type Page } from '@playwright/test'
 import AbstractPage from './abstractPage'
 
-export default class PrisonerTransactionsPage extends AbstractPage {
+export default class PrisonerMoneyPage extends AbstractPage {
   readonly heading: Locator
+
+  readonly backButton: Locator
 
   readonly tableTransactions: Locator
 
@@ -10,10 +12,11 @@ export default class PrisonerTransactionsPage extends AbstractPage {
     super(page)
     this.heading = page.locator('#prisonerTransactionsHeading')
     this.tableTransactions = page.locator('table[data-testid="prisoner-transactions-table"]')
+    this.backButton = page.locator('[data-testid="backLink"]')
   }
 
-  static async verifyOnPage(page: Page): Promise<PrisonerTransactionsPage> {
-    const prisonerTransactionsPage = new PrisonerTransactionsPage(page)
+  static async verifyOnPage(page: Page): Promise<PrisonerMoneyPage> {
+    const prisonerTransactionsPage = new PrisonerMoneyPage(page)
     await expect(prisonerTransactionsPage.heading).toBeVisible()
     return prisonerTransactionsPage
   }
--- integration_tests/specs/prisonerMoney.spec.ts
+++ integration_tests/specs/prisonerMoney.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 import { AxeBuilder } from '@axe-core/playwright'
 import { login } from '../testUtils'
-import PrisonerTransactionsPage from '../pages/prisonerMoneyPage'
+import PrisonerMoneyPage from '../pages/prisonerMoneyPage'
 import { PrisonerTransactionResponse } from '../../server/interfaces/PrisonerTransactionResponse'
 import prisonerFinanceApi from '../mockApis/prisonerFinanceApi'
 
@@ -50,12 +50,39 @@ test.describe('Prisoner Money', () => {
 
     await page.goto(`/prisoner/${prisonNumber}/money`)
 
-    const index = await PrisonerTransactionsPage.verifyOnPage(page)
-    expect(index.heading).toBeVisible()
-    expect(index.heading).toContainText("Prisoner's Transactions")
-    expect(index.tableTransactions).toBeVisible()
-    expect(index.tableTransactions.locator('thead tr th')).toHaveCount(6)
-    expect(index.tableTransactions.locator('tbody tr')).toHaveCount(payload.length)
+    const prisonerMoneyPage = await PrisonerMoneyPage.verifyOnPage(page)
+    expect(prisonerMoneyPage.heading).toBeVisible()
+    expect(prisonerMoneyPage.heading).toContainText("Prisoner's Transactions")
+    expect(prisonerMoneyPage.tableTransactions).toBeVisible()
+    expect(prisonerMoneyPage.tableTransactions.locator('thead tr th')).toHaveCount(6)
+
+    const rows = prisonerMoneyPage.tableTransactions.locator('tbody tr')
+    expect(rows).toHaveCount(payload.length)
+
+    for (let i = 0; i < payload.length; i += 1) {
+      expect(page.locator('[data-testid=row-date]').nth(i)).toHaveText(payload[i].date.toISOString())
+      expect(page.locator('[data-testid=row-description]').nth(i)).toHaveText(payload[i].description)
+      expect(page.locator('[data-testid=row-credit]').nth(i)).toHaveText(payload[i].credit.toString())
+      expect(page.locator('[data-testid=row-debit]').nth(i)).toHaveText(payload[i].debit.toString())
+      expect(page.locator('[data-testid=row-location]').nth(i)).toHaveText(payload[i].location)
+      expect(page.locator('[data-testid=row-account-type]').nth(i)).toHaveText(payload[i].accountType)
+    }
+  })
+
+  test('Backlink should render and return to index', async ({ page }) => {
+    await login(page)
+
+    await prisonerFinanceApi.stubGetPrisonerTransactionsByPrisonNumber(prisonNumber, payload)
+
+    await page.goto(`/prisoner/${prisonNumber}/money`)
+
+    const prisonerMoneyPage = await PrisonerMoneyPage.verifyOnPage(page)
+
+    expect(prisonerMoneyPage.backButton).toBeVisible()
+
+    await prisonerMoneyPage.backButton.click()
+
+    expect(new URL(page.url()).pathname).toBe('/')
   })
 
   test('Should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
@@ -65,7 +92,7 @@ test.describe('Prisoner Money', () => {
 
     await page.goto(`/prisoner/${prisonNumber}/money`)
 
-    await PrisonerTransactionsPage.verifyOnPage(page)
+    await PrisonerMoneyPage.verifyOnPage(page)
 
     const accessibilityScanResults = await new AxeBuilder({ page })
       .withTags(['wcag2a', 'wcag2aa', 'wcag22a', 'wcag22aa'])
