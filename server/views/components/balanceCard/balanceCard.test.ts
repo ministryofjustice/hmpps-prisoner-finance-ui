ca46051a597f0ffa55ea0c7b588e03214d286981
jasonwroe-mt <jason.wroe@madetech.com>
2026-03-11 15:50:54 +0000

    Added a balance card component with tests also moved card into folder

---
+++ server/views/components/balanceCard/balanceCard.test.ts
@@ -0,0 +1,35 @@
+import nunjucks from 'nunjucks'
+import * as cheerio from 'cheerio'
+import { penceToPound } from '../../../utils/utils'
+
+describe('View Components - BalanceCard', () => {
+  const njkEnv = nunjucks.configure(
+    ['server/views', 'node_modules/govuk-frontend/dist', 'node_modules/@ministryofjustice/frontend/'],
+    {
+      autoescape: true,
+      trimBlocks: true,
+      lstripBlocks: true,
+    },
+  )
+  njkEnv.addFilter('penceToPound', penceToPound)
+
+  function renderCard(params: Record<string, unknown>) {
+    const macroString = `
+      {% from "components/balanceCard/balanceCard.njk" import balanceCard %}
+      {{ balanceCard(params) }}
+    `
+    const output = njkEnv.renderString(macroString, { params })
+    return cheerio.load(output)
+  }
+
+  it('should render the heading and balance amount', () => {
+    const $ = renderCard({
+      heading: 'Test',
+      amount: 1000,
+    })
+
+    expect($('.balanceCard__heading').text().trim()).toBe('Test')
+    expect($('.balanceCard__amount').text().trim()).toBe('£10.00')
+    
+  })
+})
