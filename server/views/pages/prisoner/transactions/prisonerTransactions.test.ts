7883496532e584cc6e30451e0b27007b7a5dc084
Pietro Convalle <pietro.convalle@madetech.com>
2026-03-12 10:52:24 +0000

    Added no-transactions for tables
    Removed toBeDefined from tests

--- server/views/pages/prisoner/transactions/prisonerTransactions.test.ts
+++ server/views/pages/prisoner/transactions/prisonerTransactions.test.ts
@@ -41,10 +41,18 @@ describe('prisoner transactions page', () => {
   ]
 
   let $: cheerio.CheerioAPI
+  let njkEnv: nunjucks.Environment
   const prisonNumber = 'A12345'
+  const params = {
+    prisonNumber,
+    applicationName: 'Hmpps Prisoner Finance Ui',
+    transactions: payload,
+    prisoner: { firstName: 'BOB', lastName: 'Taylor' },
+    balance: 1000,
+  }
 
   beforeAll(() => {
-    const njkEnv = nunjucks.configure(
+    njkEnv = nunjucks.configure(
       ['server/views', 'node_modules/govuk-frontend/dist', 'node_modules/@ministryofjustice/frontend/'],
       {
         autoescape: true,
@@ -55,13 +63,7 @@ describe('prisoner transactions page', () => {
 
     setUpNunJucksFilters(njkEnv)
 
-    const html = njkEnv.render('pages/prisoner/transactions/prisonerTransactions.njk', {
-      prisonNumber,
-      applicationName: 'Hmpps Prisoner Finance Ui',
-      transactions: payload,
-      prisoner: { firstName: 'BOB', lastName: 'Taylor' },
-      balance: 1000,
-    })
+    const html = njkEnv.render('pages/prisoner/transactions/prisonerTransactions.njk', params)
 
     $ = cheerio.load(html)
   })
@@ -87,5 +89,19 @@ describe('prisoner transactions page', () => {
 
     expect($('.hmpps-summary-container__heading').text().trim()).toBe('Total')
     expect($('.hmpps-balance-card__amount').text().trim()).toBe('£10.00')
+  })
+
+  it('Should render no transactions', () => {
+    const html = njkEnv.render('pages/prisoner/transactions/prisonerTransactions.njk', {
+      ...params,
+      transactions: [],
+    })
+
+    const cheerioPage = cheerio.load(html)
+    const noTransactionsMessage = cheerioPage('[data-testid="no-transactions-message"]')
+
+    expect(cheerioPage('table[data-testid="prisoner-transactions-table"]').length).toBe(0)
+    expect(noTransactionsMessage.length).not.toBe(0)
+    expect(noTransactionsMessage.text()).toContain('No Transactions')
   })
 })
