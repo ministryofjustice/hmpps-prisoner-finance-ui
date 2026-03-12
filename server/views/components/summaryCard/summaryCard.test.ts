a60cce6962f8a83e93dbdd0a93bfe722c7b79d02
Pietro Convalle <pietro.convalle@madetech.com>
2026-03-12 11:46:48 +0000

    changed to use caller

---
+++ server/views/components/summaryCard/summaryCard.test.ts
@@ -0,0 +1,33 @@
+import nunjucks from 'nunjucks'
+import * as cheerio from 'cheerio'
+
+describe('View Components - summaryCard', () => {
+  const njkEnv = nunjucks.configure(
+    ['server/views', 'node_modules/govuk-frontend/dist', 'node_modules/@ministryofjustice/frontend/'],
+    {
+      autoescape: true,
+      trimBlocks: true,
+      lstripBlocks: true,
+    },
+  )
+
+  function renderCard(params: Record<string, unknown>) {
+    const macroString = `
+    {% from "components/summaryCard/summaryCard.njk" import summaryCard %}
+      {% call(summaryCard({ heading: "Test", data-testid="test"})) %}
+        <h1 data-testid="test-child">hello world</h1>
+      {% endcall %}
+    `
+    const output = njkEnv.renderString(macroString, { params })
+    return cheerio.load(output)
+  }
+
+  it('should render the heading and render child', () => {
+    const $ = renderCard({
+      heading: 'Test',
+    })
+
+    expect($('.hmpps-summary-card__heading').text().trim()).toBe('Test')
+    expect($('[data-testid="test-child"]').text().trim()).toBe('hello world')
+  })
+})
