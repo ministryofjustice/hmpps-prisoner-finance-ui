1137f7776c63c9c886efb00871e3184844b838d5
tristan-madetech <tristan.moss@justice.gov.uk>
2026-04-08 16:47:32 +0100

    WIP

--- server/utils/mojPaginationHelper.test.ts
+++ server/utils/mojPaginationHelper.test.ts
@@ -1,21 +1,91 @@
-import { buildPaginationItems } from './mojPaginationHelper'
+import { buildPaginationItems, calculateFirstResult, calculateLastResult } from './mojPaginationHelper'
+
+describe('calculateFirstResult', () => {
+  it('should return 0 for 1 page with 0 results', () => {
+    const result = calculateFirstResult({
+      pageNumber: 1,
+      totalElements: 0,
+      pageSize: 25,
+    })
+    expect(result).toBe(0)
+  })
+
+  it('should return 1 for 1 page with 1 result', () => {
+    const result = calculateFirstResult({
+      pageNumber: 1,
+      totalElements: 1,
+      pageSize: 25,
+    })
+    expect(result).toBe(1)
+  })
+
+  it('should return page size + 1 for page 2 ', () => {
+    const result = calculateFirstResult({
+      pageNumber: 2,
+      totalElements: 50,
+      pageSize: 25,
+    })
+    expect(result).toBe(26)
+  })
+})
+
+describe('calculateLastResult', () => {
+  it('should return total elements if on last page', () => {
+    expect(
+      calculateLastResult({
+        pageNumber: 1,
+        totalElements: 0,
+        isLastPage: true,
+        pageSize: 25,
+      }),
+    ).toBe(0)
+
+    expect(
+      calculateLastResult({
+        pageNumber: 1,
+        totalElements: 13,
+        isLastPage: true,
+        pageSize: 25,
+      }),
+    ).toBe(13)
+
+    expect(
+      calculateLastResult({
+        pageNumber: 5,
+        totalElements: 101,
+        isLastPage: true,
+        pageSize: 25,
+      }),
+    ).toBe(101)
+  })
+
+  it('should return last result number for that page when not on the last page', () => {
+    expect(
+      calculateLastResult({
+        pageNumber: 3,
+        totalElements: 125,
+        isLastPage: false,
+        pageSize: 25,
+      }),
+    ).toBe(75)
+  })
+})
 
 describe('mojPaginationHelper', () => {
   it('Should generate a pagination object with zero results', () => {
     const result = buildPaginationItems({
-      pageNumber: 0,
-      totalPages: 0,
+      pageNumber: 1,
+      totalPages: 1,
       totalElements: 0,
       content: [],
-      isLastPage: false,
+      isLastPage: true,
       pageSize: 25,
     })
 
-    expect(result.items).toEqual([])
-
-    expect(result.previous).toBeUndefined()
-    expect(result.next).toBeUndefined()
-    expect(result.results).toBeUndefined()
+    expect(result.items).toEqual([{ href: '?page=1', selected: true, text: '1' }])
+    expect(result.previous).toBeNull()
+    expect(result.next).toBeNull()
+    expect(result.results).toEqual({ count: 0, from: 0, text: 'Showing 0 to 0 of 0 total results', to: 0 })
   })
 
   it('Should generate a pagintation object with 1 result item', () => {
@@ -35,13 +105,250 @@ describe('mojPaginationHelper', () => {
         selected: true,
       },
     ])
-    expect(result.previous).toBeUndefined()
-    expect(result.next).toBeUndefined()
+    expect(result.previous).toBeNull()
+    expect(result.next).toBeNull()
     expect(result.results).toEqual({
       count: 1,
       from: 1,
       to: 1,
       text: 'Showing 1 to 1 of 1 total results',
+    })
+  })
+
+  it('Should generate a pagintation object with 3 pages', () => {
+    const result = buildPaginationItems({
+      pageNumber: 1,
+      totalPages: 3,
+      totalElements: 75,
+      content: [],
+      isLastPage: false,
+      pageSize: 25,
+    })
+
+    expect(result.items).toEqual([
+      {
+        text: '1',
+        href: '?page=1',
+        selected: true,
+      },
+      {
+        text: '2',
+        href: '?page=2',
+        selected: false,
+      },
+      {
+        text: '3',
+        href: '?page=3',
+        selected: false,
+      },
+    ])
+    expect(result.previous).toBeNull()
+    expect(result.next.text).toBe('Next')
+    expect(result.next.href).toBe('?page=2')
+    expect(result.results).toEqual({
+      count: 75,
+      from: 1,
+      to: 25,
+      text: 'Showing 1 to 25 of 75 total results',
+    })
+  })
+
+  // Handle 4 pages
+  it('Should generate a pagintation object with 4 pages', () => {
+    // We should expect to see 1,2,3,4 not 1,2,3,...,4
+    const result = buildPaginationItems({
+      pageNumber: 1,
+      totalPages: 4,
+      totalElements: 100,
+      content: [],
+      isLastPage: false,
+      pageSize: 25,
+    })
+
+    expect(result.items).toEqual([
+      {
+        text: '1',
+        href: '?page=1',
+        selected: true,
+      },
+      {
+        text: '2',
+        href: '?page=2',
+        selected: false,
+      },
+      {
+        text: '3',
+        href: '?page=3',
+        selected: false,
+      },
+      {
+        text: '4',
+        href: '?page=4',
+        selected: false,
+      },
+    ])
+    expect(result.previous).toBeNull()
+    expect(result.next.text).toBe('Next')
+    expect(result.next.href).toBe('?page=2')
+    expect(result.results).toEqual({
+      count: 100,
+      from: 1,
+      to: 25,
+      text: 'Showing 1 to 25 of 100 total results',
+    })
+  })
+
+  // Handle 5 pages with a ellipses
+  it('Should generate a pagintation object with 5 pages', () => {
+    // We should expect to see 1,2,3,...,5
+    const result = buildPaginationItems({
+      pageNumber: 1,
+      totalPages: 5,
+      totalElements: 125,
+      content: [],
+      isLastPage: false,
+      pageSize: 25,
+    })
+
+    expect(result.items).toEqual([
+      {
+        text: '1',
+        href: '?page=1',
+        selected: true,
+      },
+      {
+        text: '2',
+        href: '?page=2',
+        selected: false,
+      },
+      {
+        text: '3',
+        href: '?page=3',
+        selected: false,
+      },
+      {
+        type: 'dots',
+      },
+      {
+        text: '5',
+        href: '?page=5',
+        selected: false,
+      },
+    ])
+    expect(result.previous).toBeNull()
+    expect(result.next.text).toBe('Next')
+    expect(result.next.href).toBe('?page=2')
+    expect(result.results).toEqual({
+      count: 125,
+      from: 1,
+      to: 25,
+      text: 'Showing 1 to 25 of 125 total results',
+    })
+  })
+
+  // ellipses in front of our position
+  it('Should generate a pagintation object with 5 pages when on page 4', () => {
+    // We should expect to see 1,...,3,4,5
+    const result = buildPaginationItems({
+      pageNumber: 4,
+      totalPages: 5,
+      totalElements: 125,
+      content: [],
+      isLastPage: false,
+      pageSize: 25,
+    })
+
+    expect(result.items).toEqual([
+      {
+        text: '1',
+        href: '?page=1',
+        selected: false,
+      },
+      {
+        type: 'dots',
+      },
+      {
+        text: '3',
+        href: '?page=3',
+        selected: false,
+      },
+      {
+        text: '4',
+        href: '?page=4',
+        selected: true,
+      },
+      {
+        text: '5',
+        href: '?page=5',
+        selected: false,
+      },
+    ])
+    expect(result.previous.text).toBe('Previous')
+    expect(result.previous.href).toBe('?page=3')
+    expect(result.next.text).toBe('Next')
+    expect(result.next.href).toBe('?page=5')
+    expect(result.results).toEqual({
+      count: 125,
+      from: 76,
+      to: 100,
+      text: 'Showing 76 to 100 of 125 total results',
+    })
+  })
+
+  // ellipses in fron and behind our position
+  it('Should generate a pagintation object with elipses on both sides of the middle section when it doesnt contain the first or last page', () => {
+    // We should expect to see 1,...,3,4,5,...7
+    const result = buildPaginationItems({
+      pageNumber: 4,
+      totalPages: 7,
+      totalElements: 175,
+      content: [],
+      isLastPage: false,
+      pageSize: 25,
+    })
+
+    expect(result.items).toEqual([
+      {
+        text: '1',
+        href: '?page=1',
+        selected: false,
+      },
+      {
+        type: 'dots',
+      },
+      {
+        text: '3',
+        href: '?page=3',
+        selected: false,
+      },
+      {
+        text: '4',
+        href: '?page=4',
+        selected: true,
+      },
+      {
+        text: '5',
+        href: '?page=5',
+        selected: false,
+      },
+      {
+        type: 'dots',
+      },
+      {
+        text: '7',
+        href: '?page=7',
+        selected: false,
+      },
+    ])
+    expect(result.previous.text).toBe('Previous')
+    expect(result.previous.href).toBe('?page=3')
+    expect(result.next.text).toBe('Next')
+    expect(result.next.href).toBe('?page=5')
+    expect(result.results).toEqual({
+      count: 175,
+      from: 76,
+      to: 100,
+      text: 'Showing 76 to 100 of 175 total results',
     })
   })
 })
--- server/utils/mojPaginationHelper.ts
+++ server/utils/mojPaginationHelper.ts
@@ -1,10 +1,10 @@
 import { Page } from '../interfaces/Pageable'
 
 export interface mojPagination {
-  items: (mojPaginationItem | mojPaginationDots)[]
+  items: Array<mojPaginationItem | mojPaginationDots>
   results?: mojPaginationResult
-  previous?: mojPaginationArrowItem
-  next?: mojPaginationArrowItem
+  previous: mojPaginationArrowItem | null
+  next: mojPaginationArrowItem | null
 }
 
 interface mojPaginationItem {
@@ -26,39 +26,98 @@ interface mojPaginationArrowItem {
 }
 
 interface mojPaginationDots {
-  type: 'dots'
+  type: string
 }
 
-/*
-const buildItems = (count: number) => {
-   
-    for()
-        text: string,
-        href: string,
-        selected: boolean,
-
+export const calculateFirstResult = ({
+  pageNumber,
+  pageSize,
+  totalElements,
+}: {
+  pageNumber: number
+  pageSize: number
+  totalElements: number
+}) => {
+  const firstEntryForPage = (pageNumber - 1) * pageSize + 1
+  return Math.min(firstEntryForPage, totalElements)
 }
-*/
 
-export const buildPaginationItems = <T>(pageObject: Page<T>): mojPagination => {
-  if (pageObject.totalElements === 0) {
-    return {
-      items: [],
+export const calculateLastResult = ({
+  pageNumber,
+  pageSize,
+  totalElements,
+  isLastPage,
+}: {
+  pageNumber: number
+  pageSize: number
+  totalElements: number
+  isLastPage: boolean
+}) => {
+  return isLastPage ? totalElements : pageSize * pageNumber
+}
+
+export const buildPaginationItems = <T>({
+  pageNumber,
+  pageSize,
+  totalPages,
+  isLastPage,
+  totalElements,
+}: Page<T>): mojPagination => {
+  const renderedMiddle = [pageNumber - 1, pageNumber, pageNumber + 1]
+
+  if (pageNumber === 1) {
+    renderedMiddle.push(3)
+  }
+
+  const filteredMiddle = renderedMiddle.filter(n => n > 0 && n <= totalPages)
+
+  const items = []
+
+  if (!filteredMiddle.includes(1)) {
+    items.push({
+      text: '1',
+      href: '?page=1',
+      selected: false,
+    })
+
+    if (totalPages > 4) {
+      items.push({ type: 'dots' })
     }
   }
+
+  filteredMiddle.forEach(n => {
+    const item = {
+      text: `${n}`,
+      href: `?page=${n}`,
+      selected: pageNumber === n,
+    }
+    items.push(item)
+  })
+
+  if (!filteredMiddle.includes(totalPages)) {
+    if (totalPages > 4) {
+      items.push({ type: 'dots' })
+    }
+
+    items.push({
+      text: `${totalPages}`,
+      href: `?page=${totalPages}`,
+      selected: false,
+    })
+  }
+
+  const from = calculateFirstResult({ pageNumber, pageSize, totalElements })
+  const to = calculateLastResult({ isLastPage, pageNumber, pageSize, totalElements })
+
   return {
-    items: [
-      {
-        text: '1',
-        href: '?page=1',
-        selected: true,
-      },
-    ],
+    items,
     results: {
-      count: 1,
-      from: 1,
-      to: 1,
-      text: 'Showing 1 to 1 of 1 total results',
+      count: totalElements,
+      from,
+      to,
+      text: `Showing ${from} to ${to} of ${totalElements} total results`,
     },
+    next: isLastPage ? null : { text: 'Next', href: `?page=${pageNumber + 1}` },
+    previous: pageNumber === 1 ? null : { text: 'Previous', href: `?page=${pageNumber - 1}` },
   }
 }
