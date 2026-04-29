132e4bf27416dcb97d8804e666db578b7de4c296
jasonwroe-mt <jason.wroe@madetech.com>
2026-04-29 12:14:02 +0100

    Rename and move session type extension

--- server/routes/store/sessionStorageExtensions.ts
+++ server/routes/store/sessionStorageExtensions.ts
@@ -1,5 +1,6 @@
 import CreditAPrisonerForm from '../../classes/creditAPrisonerForm'
 
+// Express-session does not have type inference, so this extends the object for our form data
 declare module 'express-session' {
   interface SessionData {
     creditForm?: CreditAPrisonerForm
