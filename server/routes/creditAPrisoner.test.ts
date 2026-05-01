15fcffc219542fdb2f907fcd8a7f3549dbeba2fd
sam.caine <sam.caine@justice.gov.uk>
2026-05-01 11:58:27 +0100

    fix linting

--- server/routes/creditAPrisoner.test.ts
+++ server/routes/creditAPrisoner.test.ts
@@ -1,18 +1,12 @@
 import type { Express } from 'express'
 import { PrisonerMoneyPermission, PermissionsService } from '@ministryofjustice/hmpps-prison-permissions-lib'
 
-import signature from 'cookie-signature'
 import request from 'supertest'
 import AuditService, { AuditPage } from '../services/auditService'
 import mockPermissions from './testutils/mockPermissions'
 import { appWithAllRoutes, user } from './testutils/appSetup'
-import config from '../config'
 
-import CreditAPrisonerService from '../services/creditAPrisonerService'
 import PrisonerSearchService from '../services/prisonerSearchService'
-import ExpressSessionAdapter from './store/expressSessionAdapter'
-import InMemoryStore from './store/inMemoryStore'
-import CreditAPrisonerForm from '../classes/creditAPrisonerForm'
 
 jest.mock('../services/auditService')
 jest.mock('../services/prisonerSearchService')
@@ -21,7 +15,6 @@ jest.mock('@ministryofjustice/hmpps-prison-permissions-lib')
 const auditService = new AuditService(null) as jest.Mocked<AuditService>
 const prisonerSearchService = new PrisonerSearchService(null) as jest.Mocked<PrisonerSearchService>
 const prisonPermissionsService = {} as unknown as PermissionsService
-const expressSessionAdapter = new ExpressSessionAdapter(new InMemoryStore()) as jest.Mocked<ExpressSessionAdapter>
 
 let app: Express
 
@@ -60,30 +53,6 @@ describe('/credit-a-prisoner', () => {
   afterEach(() => {
     jest.resetAllMocks()
   })
-
-  async function createCookieForSession(formData: CreditAPrisonerForm) {
-    const fakeSessionId = 'test-session-999'
-    const { secret } = config.session
-
-    // 2. Seed your in-memory store directly!
-    // We bypass Express entirely and write straight to the adapter
-    await new Promise(resolve => {
-      expressSessionAdapter.set(
-        fakeSessionId,
-        {
-          cookie: { originalMaxAge: 86400000 },
-          returnTo: '',
-          creditForm: formData, // The specific state you want to test
-        },
-        resolve,
-      )
-    })
-
-    // 3. Cryptographically sign the cookie exactly how express-session does it
-    // express-session strictly requires the "s:" prefix to know it's a signed cookie
-    const signedCookieValue = `s:${signature.sign(fakeSessionId, secret)}`
-    return `connect.sid=${signedCookieValue}`
-  }
 
   describe('/credit-to', () => {
     describe('GET', () => {
