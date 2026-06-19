ef5d8de193d009311eaab0f38dc491f1e61381c2
sam-caine-moj <sam.caine@justice.gov.uk>
2026-06-19 16:47:59 +0100

    Pfi 1250 delete hold balance summary card for proxima centauri (#116)
    * remove holds card
    
    * remove current balance card

--- server/views/pages/prisoner/transactions/prisonerTransactions.test.ts
+++ server/views/pages/prisoner/transactions/prisonerTransactions.test.ts
@@ -151,12 +151,6 @@ describe('prisoner transactions page', () => {
     expect(transactionsTable.find('thead tr th').length).toBe(6)
     expect(transactionsTable.find('tbody tr').length).toBe(payload.length)
 
-    expect($('[data-testid="view-prisoner-current-balance-card_header"]').text().trim()).toBe('Current balance')
-    expect($('[data-testid="view-prisoner-current-balance-card_amount"]').text().trim()).toBe('£10.00')
-
-    expect($('[data-testid="view-prisoner-hold-balance-card_header"]').text().trim()).toBe('Hold balance')
-    expect($('[data-testid="view-prisoner-hold-balance-card_amount"]').text().trim()).toBe('£0.00')
-
     expect($('[data-testid="view-prisoner-total-balance-card_header"]').text().trim()).toBe('Total balance')
     expect($('[data-testid="view-prisoner-total-balance-card_amount"]').text().trim()).toBe('£10.00')
 
