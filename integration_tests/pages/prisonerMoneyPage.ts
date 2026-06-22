50c0e69b982053022efa9e50bb9db42d36b13cc0
emilyfbgov <emily.bennett@justice.gov.uk>
2026-06-22 09:57:51 +0100

    revert current balance card removal (#121)

--- integration_tests/pages/prisonerMoneyPage.ts
+++ integration_tests/pages/prisonerMoneyPage.ts
@@ -8,6 +8,8 @@ export default class PrisonerMoneyPage extends AbstractPage {
 
   readonly tableTransactions: Locator
 
+  readonly currentBalanceCard: Locator
+
   readonly totalBalanceCard: Locator
 
   readonly prisonerInformationHeader: Locator
@@ -31,6 +33,7 @@ export default class PrisonerMoneyPage extends AbstractPage {
     this.heading = page.locator('#prisonerTransactionsHeading')
     this.tableTransactions = page.locator('table[data-testid="prisoner-transactions-table"]')
     this.backButton = page.locator('[data-testid="backLink"]')
+    this.currentBalanceCard = page.locator('[data-testid="view-prisoner-current-balance-card"]')
     this.totalBalanceCard = page.locator('[data-testid="view-prisoner-total-balance-card"]')
     this.prisonerInformationHeader = page.locator('[data-testid="hmpps-profile-banner"]')
     this.topPagination = page.locator('#top-pagination')
